e604beff1c6223330ee61b0daf970bf552cfe1ea
Rohn Allen Ramos Santiago <rsantiag@tcd.ie>
2023-11-15 15:50:43 +0000

    initial commit

---
+++ studybot/src/test/java/ie/tcd/scss/studybot/StudybotApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.studybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
