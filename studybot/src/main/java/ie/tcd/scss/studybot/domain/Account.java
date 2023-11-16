374cf1712c059ae7660984c56e7eb5000f065a7c
Rohn Allen Ramos Santiago <rsantiag@tcd.ie>
2023-11-16 19:57:22 +0000

    Added constructors, getter and setters to account class

--- studybot/src/main/java/ie/tcd/scss/studybot/domain/Account.java
+++ studybot/src/main/java/ie/tcd/scss/studybot/domain/Account.java
@@ -19,5 +19,45 @@ public class Account {
     @OneToMany
     private ArrayList<Module> modules;
 
+    public Account(String email, String name, String username, ArrayList<Module> modules) {
+        this.email = email;
+        this.name = name;
+        this.username = username;
+        this.modules = modules;
+    }
 
+    public Account() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(ArrayList<Module> modules) {
+        this.modules = modules;
+    }
 }
