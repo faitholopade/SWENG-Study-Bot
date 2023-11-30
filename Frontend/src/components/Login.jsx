2776fa5ea22989ac0f5114d9b80ea39532b77675
Mahirm01 <masudm@tcd.ie>
2023-11-30 23:30:51 +0000

    ok users can now register an account. Login logic side of things finished

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -2,7 +2,27 @@ import React, { useState } from 'react';
 
 const Login = () => {
     const [username, setUsername] = useState('');
+    const [regUsername, setRegUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [regPassword, setRegPassword] = useState('');
+    const [showRegister, setShowRegister] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [registrationSuccess, setRegistrationSuccess] = useState('');
+
+    const resetForm = () => {
+       setName('');
+       setUsername('');
+       setRegUsername('');
+       setEmail('');
+       setPassword('');
+       setRegPassword('');
+       setRegistrationSuccess('');
+    };
+    const showRegistrationForm = () => {
+    setRegistrationSuccess('');
+    setShowRegister(true);
+    };
 
 const handleLogin = () => {
 /* fetch('http://localhost:8080/account/hello')
@@ -46,6 +66,42 @@ const handleLogin = () => {
     });
 };
 
+const handleRegister = () => {
+    fetch('http://localhost:8080/account/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            account: {
+                name: name,
+                username: regUsername,
+                email: email,
+                password: regPassword
+            }
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        return response.text();
+    })
+    .then(data => {
+        console.log('Registration successful', data);
+        setRegistrationSuccess('Registration successful');
+        setTimeout(() => {
+                    resetForm(); // Reset form fields after showing the success message for a while
+                    setShowRegister(false); // Hide the form
+                }, 5000); // 3 seconds delay
+    })
+    .catch(error => {
+        console.error('There was a problem with the registration operation:', error);
+    });
+};
+
+
     return (
         <div>
         <div className="top-banner">
@@ -60,13 +116,28 @@ const handleLogin = () => {
                     <div>Milena </div>
                     <div>Dario </div>
                     </div>
+                    </div>
+                    <div className="register regH" onMouseOver={() => setShowRegister(true)} onMouseOut={() => registrationSuccess && setTimeout(() => setShowRegister(false), 5000)}>
+                      Register
+                      {showRegister && (
+                            <div className="register-form regS">
+                            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+                            <input type="text" value={regUsername} onChange={(e) => setRegUsername(e.target.value)} placeholder="Username" />
+                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                            <input type="password" value={regPassword} onChange={(e) => setRegPassword(e.target.value)} placeholder="Password" />
+                            <button onClick={handleRegister}>Register</button>
+                            {registrationSuccess && <div className="success-message">{registrationSuccess}</div>}
+                            </div>
+
+                       )}
+
                 </div>
             </div>
 
             <div className="LoginBox">
                 <h2 className ="Logins">Login</h2>
                 <form>
-                    <label className="login-label">Username: </label>
+                    <label className="login-label">Email: </label>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
                     <br />
                     <label className="login-label">Password: </label>
