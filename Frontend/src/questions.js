f2e2752943b52a35b765719d76eb2f4e576de997
dcipani <dcipani@gmail.com>
2023-12-02 19:00:45 +0000

    initial commit

---
+++ Frontend/src/questions.js
@@ -0,0 +1,18 @@
+export const questions =
+    [
+        { 
+            question: "q1", 
+            options: ["a", "b", "c", "d"],
+            answer: "a"
+        }, 
+        { 
+            question: "q2", 
+            options: ["a", "b", "c", "d"],
+            answer: "b"
+        }, 
+        { 
+            question: "q3", 
+            options: ["a", "b", "c", "d"],
+            answer: "c"
+        }
+    ]
