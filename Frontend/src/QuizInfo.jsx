20705d386a2633f27684630ea4df1e22f52d7993
dcipani <dcipani@gmail.com>
2023-12-03 02:25:16 +0000

    implemented basic scoring

--- Frontend/src/QuizInfo.jsx
+++ Frontend/src/QuizInfo.jsx
@@ -4,16 +4,16 @@ import { useState } from 'react';
 const QuizInfo = () => {
     const [questionAt, setQuestionAt] = useState(0);
     const [score, setScore] = useState(0);
+    const [quizFinished, setQuizFinished] = useState(false);
     const {question, options, answer} = questions[questionAt];
     const optionSelected = (option) => {
         if (option === questions[questionAt].answer) {
-            setScore(score + 1);
+            setScore(score+1);
         }
         if (questionAt < questions.length-1) {
             setQuestionAt(questionAt + 1);
         } else {
-            return;
-            //show result
+            setQuizFinished(true);
         }
     };
 
@@ -33,16 +33,21 @@ const QuizInfo = () => {
                     </div>
                 </div>
             </div>
-
-            <div className="quiz-box">
-                <div className="questions-completed">Question {questionAt+1} of {questions.length}:</div>
-                <h3 className="question-text">{question}</h3>
-                <div className='button-grid'>
-                    {questions[questionAt].options.map((option) => (
-                        <button className='answer-button' onClick={() => optionSelected(option)}>{option}</button>
-                    ))}
+            {(!quizFinished) ? (
+                <div className="quiz-box">
+                    <div className="questions-completed">Question {questionAt+1} of {questions.length}:</div>
+                    <h3 className="question-text">{question}</h3>
+                    <div className='button-grid'>
+                        {questions[questionAt].options.map((option) => (
+                            <button className='answer-button' onClick={() => optionSelected(option)}>{option}</button>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className="quiz-box">
+                    <h3 className="question-text">Score: {score}/{questions.length}</h3>
+                </div>
+            )}
             <img src="/images/quizbot mascot.png" alt="Description" className="login-image" />
 
         </div>
--- Frontend/src/questions.js
+++ Frontend/src/questions.js
@@ -1,18 +1,18 @@
 export const questions =
     [
-        { 
-            question: "q1", 
-            options: ["a", "b", "c", "d"],
-            answer: "a"
-        }, 
-        { 
-            question: "q2", 
-            options: ["a", "b", "c", "d"],
-            answer: "b"
-        }, 
-        { 
-            question: "q3", 
-            options: ["a", "b", "c", "d"],
-            answer: "c"
+        {
+            question: "What is the answer to question 1?",
+            options: ["Option A", "Option B", "Option C", "Option D"],
+            answer: "Option A"
+        },
+        {
+            question: "What is the answer to question 2?",
+            options: ["Option A", "Option B", "Option C", "Option D"],
+            answer: "Option B"
+        },
+        {
+            question: "What is the answer to question 3?",
+            options: ["Option A", "Option B", "Option C", "Option D"],
+            answer: "Option C"
         }
     ]
