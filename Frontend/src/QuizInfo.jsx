8257b15d8ec81b986a01b8b054e05d264d04ce16
dcipani <dcipani@gmail.com>
2023-12-03 13:03:28 +0000

    added timer and refactored quiz interface

--- Frontend/src/QuizInfo.jsx
+++ Frontend/src/QuizInfo.jsx
@@ -1,6 +1,6 @@
 import {questions} from './questions';
-import { useState } from 'react';
-
+import { useState , useEffect } from 'react';
+// import Timer from './Timer'
 const QuizInfo = () => {
     const [questionAt, setQuestionAt] = useState(0);
     const [score, setScore] = useState(0);
@@ -11,13 +11,35 @@ const QuizInfo = () => {
         if (option == questions[questionAt].answer) {
             setScore(score+1);
         }
+        nextQuestion();
+
+    };
+
+    const Timer = ({timeElapsed}) => {
+        const [time, setTime] = useState(0);
+        const [maxTime, setMaxTime] = useState(20)
+        useEffect(() => {
+            if (time <= maxTime) {
+                const interval = setInterval(() => {
+                    setTime(prev => prev+.01);
+                }, 10);
+
+                return () => clearInterval(interval);
+            } else {
+                setTime(0);
+                timeElapsed();
+            }
+        }, [time]);
+
+        return <div className='timer-bar' style={{width: `${((time/maxTime)*100)}%`}}></div>;
+    };
+    const nextQuestion = () => {
         if (questionAt < questions.length-1) {
             setQuestionAt(questionAt + 1);
         } else {
             setQuizFinished(true);
         }
-    };
-
+    }
     const retry = () => {
         setQuizStarted(false);
         setQuizFinished(false);
@@ -28,7 +50,7 @@ const QuizInfo = () => {
     return (
         <div>
             <div className="top-banner">
-                Group 8 Quizbot
+                Group 8 Quiz Bot
                 <div className="group-members">
                     Group Members
                     <div className="member-list">
@@ -43,7 +65,9 @@ const QuizInfo = () => {
             </div>
             {(quizStarted) ? (
                 (!quizFinished) ? (
+
                     <div className="quiz-box">
+                        <Timer timeElapsed = {nextQuestion}/>
                         <div className="questions-completed">Question {questionAt+1} of {questions.length}:</div>
                         <h3 className="question-text">{question}</h3>
                         <div className='button-grid'>
@@ -52,23 +76,24 @@ const QuizInfo = () => {
                             ))}
                         </div>
                     </div>
+
                 ) : (
                     <div className="quiz-box">
                         <h3 className="question-text">Score: {(score/questions.length).toFixed(2)*100}%</h3>
-                        <h3 className="question-text">You got {score} of {questions.length} questions correct!</h3>
-                        <button className='start-button' onClick={() => retry()}>Try Again!</button>
+                        <h3 className="question-text">You got {score} out of {questions.length} questions correct!</h3>
+                        <button className='quiz-start-button' onClick={() => retry()}>Try Again!</button>
 
                     </div>
                 )
             ) : (
                 <div className="quiz-box">
-                    <h2>Quiz Topic</h2>
-                    <button className='start-button' onClick={() => setQuizStarted(true)}>Start!</button>
+                    <h3 className="question-text">Quiz Topic</h3>
+                    <h3 className="question-text">Time per Question: 20s</h3>
+                    <h3 className="question-text">Number of Questions: {questions.length}</h3>
+                    <button className='quiz-start-button' onClick={() => setQuizStarted(true)}>Start!</button>
                 </div>)
             }
             <img src="/images/quizbot mascot.png" alt="Description" className="login-image" />
-
-
         </div>
 
     );}
--- Frontend/src/questions.js
+++ Frontend/src/questions.js
@@ -1,17 +1,17 @@
 export const questions =
     [
         {
-            question: "What is the answer to question 1?",
+            question: "What is the answer to question 1? (it's A)",
             options: ["Option A", "Option B", "Option C", "Option D"],
             answer: "Option A"
         },
         {
-            question: "What is the answer to question 2?",
+            question: "What is the answer to question 2? (it's B)",
             options: ["Option A", "Option B", "Option C", "Option D"],
             answer: "Option B"
         },
         {
-            question: "What is the answer to question 3?",
+            question: "What is the answer to question 3? (it's C)",
             options: ["Option A", "Option B", "Option C", "Option D"],
             answer: "Option C"
         }
