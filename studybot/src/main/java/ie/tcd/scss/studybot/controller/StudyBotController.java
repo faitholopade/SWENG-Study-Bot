d57c854fa25a6a30a4afbf1c61be00adfba4a305
Sabjel Pasha <86008235+Sabbbbb@users.noreply.github.com>
2023-12-04 19:00:05 +0000

    Added support module selection functionality

--- studybot/src/main/java/ie/tcd/scss/studybot/controller/StudyBotController.java
+++ studybot/src/main/java/ie/tcd/scss/studybot/controller/StudyBotController.java
@@ -3,12 +3,13 @@ package ie.tcd.scss.studybot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import ie.tcd.scss.studybot.domain.AnswerFrom;
+import ie.tcd.scss.studybot.domain.Modules;
 import ie.tcd.scss.studybot.domain.PromptTo;
 
 @RestController
@@ -24,9 +25,26 @@ public class StudyBotController {
     @Autowired
     private RestTemplate template;
 
-    @GetMapping("/study")
-    public String chat(@RequestParam("prompt") String prompt){
-        PromptTo request = new PromptTo(AiModel, prompt);
+    @GetMapping("/{module}")
+    public String chat(@PathVariable String module){
+        String modulePrompt = "";
+        switch (module.toLowerCase()) {
+            case "networks":
+                modulePrompt = Modules.NETWORKS;
+                break;
+            case "functional":
+                modulePrompt = Modules.FUNCTIONAL;
+                break;
+            case "architecture":
+                modulePrompt = Modules.ARCHITECTURE;
+                break;
+            case "sweng":
+                modulePrompt = Modules.SWENG;
+                break;
+            default:
+                return "Your url must match an existing module (networks, functional, architecture, sweng)";
+        }
+        PromptTo request = new PromptTo(AiModel, modulePrompt);
         AnswerFrom answer = template.postForObject(url, request, AnswerFrom.class);
         if(answer == null) {
             return "Null Response";
