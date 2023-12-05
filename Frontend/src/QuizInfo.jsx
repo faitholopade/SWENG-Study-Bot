c4c739c8be0b0817ee17ace7e74ec9c7f2c8194e
Dario Cipani <cipanid@tcd.ie>
2023-12-05 02:14:20 +0000

    implemented working fetch method and read in .json info to create actual quiz questions

--- Frontend/src/QuizInfo.jsx
+++ Frontend/src/QuizInfo.jsx
@@ -1,36 +1,37 @@
-// import {questions} from './questions';
 import { useState , useEffect } from 'react';
-import QuestionDS from './QuestionInterface'
 const QuizInfo = ({moduleName}) => {
-    const [questions, setQuestions] = useState([]);
+    const [questions, setQuestions] = useState(null);
 
     useEffect(() => {
-        const fetchQuestions = async() => {
-            const res = await fetch(`http://localhost:8080/${moduleName}`);
-            const data = await res.json();
-            const structData = data.map((item) => ({
-                question: item.question,
-                options: item.options,
-                answer: item.answer
-            }));
-            setQuestions(structData);
-        }
+        const fetchQuestions = async () => {
+            try {
+                const res = await fetch(`http://localhost:8080/${moduleName}`);
+                if (!res.ok) {
+                    throw new Error('no network response');
+                }
+                const data = await res.json();
+                console.log(data)
+                if (questions == null)
+                    setQuestions(data);
+            } catch (error) {
+                console.error('error fetching data:', error);
+            }
+        };
+    
         fetchQuestions();
     }, []);
-
 
     const [questionAt, setQuestionAt] = useState(0);
     const [score, setScore] = useState(0);
     const [quizStarted, setQuizStarted] = useState(false);
     const [quizFinished, setQuizFinished] = useState(false);
-    const {question, options, answer} = questions[questionAt];
     const optionSelected = (option) => {
-        if (option == questions[questionAt].answer) {
-            setScore(score+1);
-        }
+		if (option == questions[questionAt].answer) {
+			setScore(score+1);
+		}
         nextQuestion();
-
-    };
+		
+	};
 
     const Timer = ({timeElapsed}) => {
         const [time, setTime] = useState(0);
@@ -38,22 +39,22 @@ const QuizInfo = ({moduleName}) => {
         useEffect(() => {
             if (time <= maxTime) {
                 const interval = setInterval(() => {
-                    setTime(prev => prev+.01);
+                setTime(prev => prev+.01);
                 }, 10);
-
+                
                 return () => clearInterval(interval);
             } else {
                 setTime(0);
                 timeElapsed();
             }
         }, [time]);
-
+        
         return <div className='timer-bar' style={{width: `${100-((time/maxTime)*100)}%`}}></div>;
     };
     const nextQuestion = () => {
         if (questionAt < questions.length-1) {
             setQuestionAt(questionAt + 1);
-        } else {
+		} else {
             setQuizFinished(true);
         }
     }
@@ -67,60 +68,66 @@ const QuizInfo = ({moduleName}) => {
     const exit = () => {
         //return to main menu
     }
-
+    
     return (
         <div>
-            <div className="top-banner">
+        <div className="top-banner">
                 Group 8 Quiz Bot
                 <div className="group-members">
                     Group Members
-                    <div className="member-list">
-                        <div>Faith</div>
-                        <div>Mahir </div>
-                        <div>Rohn</div>
-                        <div>Sab </div>
-                        <div>Milena </div>
-                        <div>Dario </div>
-                    </div>
+                <div className="member-list">
+                    <div>Faith</div>
+                    <div>Mahir </div>
+                    <div>Rohn</div>
+                    <div>Sab </div>
+                    <div>Milena </div>
+                    <div>Dario </div>
                 </div>
             </div>
+        </div>
             {(quizStarted) ? (
                 (!quizFinished) ? (
-
+                    
                     <div className="quiz-box">
                         <Timer timeElapsed = {nextQuestion}/>
-                        <div className="questions-completed">Question {questionAt+1} of {questions.length}:</div>
-                        <h3 className="question-text">{question}</h3>
+                        <div className="questions-completed">Question {questionAt+1} of 4:</div>
+                        <h3 className="question-text">{questions[questionAt].question}</h3>
                         <div className='button-grid'>
                             {questions[questionAt].options.map((option) => (
                                 <button className='answer-button' onClick={() => optionSelected(option)}>{option}</button>
                             ))}
                         </div>
                     </div>
-
+                   
                 ) : (
                     <div className="quiz-box">
-                        <h3 className="question-text">Score: {(score/questions.length).toFixed(2)*100}%</h3>
-                        <h3 className="question-text">You got {score} out of {questions.length} questions correct!</h3>
+                        <h3 className="question-text">Score: {(score/4).toFixed(2)*100}%</h3>
+                        <h3 className="question-text">You got {score} out of 4 questions correct!</h3>
                         <button className='quiz-start-button' onClick={() => retry()}>Try Again!</button>
                         <button className='quiz-start-button' onClick={() => exit()}>Exit</button>
 
                     </div>
                 )
-            ) : (
+            ) : ( 
                 <div className="quiz-box">
                     <h3 className="question-text">Quiz Topic</h3>
                     <h3 className="question-text">Time per Question: 15s</h3>
-                    <h3 className="question-text">Number of Questions: {questions.length}</h3>
-                    <button className='quiz-start-button' onClick={() => setQuizStarted(true)}>Start!</button>
+                    <h3 className="question-text">Number of Questions: 4</h3>
+                    {(questions != null) ? (   
+                            
+                        <button className='quiz-start-button' onClick={() => setQuizStarted(true)}>Start!</button>
+                        
+                        ) : (
+                        <h3 className='question-text'> Loading...</h3>
+                    )}
                 </div>)
             }
-            <img src="/images/quizbot mascot.png" alt="Description" className="login-image" />
+            <img src="/images/quizbot mascot.png" alt="Description" className="login-image" /> 
         </div>
-
+        
     );}
-
-
-
+   
+    
+        
 
 export default QuizInfo;
